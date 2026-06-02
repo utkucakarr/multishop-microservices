d30a3f35ee8b826b3c04007e56454951a46a9d11
Utku Çakar <cakarutku1@hotmail.com>
2026-06-02 22:11:35 +0300

    refactor: apply rich domain model to ProductDetail entity"

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -1,5 +1,7 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MultiShop.Catalog.Entities.ValueObject;
+using MultiShop.Catalog.Exceptions;
 
 namespace MultiShop.Catalog.Entities
 {
@@ -7,15 +9,38 @@ namespace MultiShop.Catalog.Entities
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string ProductDetailId { get; set; }
-
-        public string ProductDescription { get; set; }
-
-        public string ProductInfo { get; set; }
-
-        public string ProductId { get; set; }
+        public string ProductDetailId { get; private set; }
+        public string ProductId { get; private set; }
+        public string ShortDescription { get; private set; }
+        public string LongDescription { get; private set; }
+        public string ProductInfo { get; private set; }
 
         [BsonIgnore]
         public Product Product { get; set; }
+
+        [BsonConstructor]
+        private ProductDetail() { }
+
+        public static ProductDetail Create(ProductDetailCreationParameters parameters)
+        {
+            return new ProductDetail
+            {
+                ProductDetailId = ObjectId.GenerateNewId().ToString(),
+                ProductId = parameters.ProductId,
+                ShortDescription = parameters.ShortDescription,
+                LongDescription = parameters.LongDescription,
+                ProductInfo = parameters.ProductInfo
+            };
+        }
+
+        public void UpdateDetails(string shortDescription, string longDescription, string productInfo)
+        {
+            if (string.IsNullOrWhiteSpace(shortDescription))
+                throw new CatalogDomainException("Short description cannot be empty.");
+
+            ShortDescription = shortDescription;
+            LongDescription = longDescription;
+            ProductInfo = productInfo;
+        }
     }
 }
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ValueObject/ProductDetailCreationParameters.cs
@@ -0,0 +1,29 @@
+﻿using MultiShop.Catalog.Exceptions;
+
+namespace MultiShop.Catalog.Entities.ValueObject
+{
+    public record ProductDetailCreationParameters
+    {
+        public string ProductId { get; init; }
+        public string ShortDescription { get; init; }
+        public string LongDescription { get; init; }
+        public string ProductInfo { get; init; }
+
+        public ProductDetailCreationParameters(
+           string productId,
+           string shortDescription,
+           string longDescription,
+           string productInfo)
+        {
+            if (string.IsNullOrWhiteSpace(productId))
+                throw new CatalogDomainException("ProductId cannot be empty.");
+            if (string.IsNullOrWhiteSpace(shortDescription))
+                throw new CatalogDomainException("Short description cannot be empty.");
+
+            ProductId = productId;
+            ShortDescription = shortDescription;
+            LongDescription = longDescription;
+            ProductInfo = productInfo;
+        }
+    }
+}
